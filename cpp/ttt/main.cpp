ebf4eb85eadcded6cf2e2b9af376e7dd902816f9
Ivan Wang <113294786+Ivan-Wang-J-W@users.noreply.github.com>
2024-02-16 18:10:47 +0800

    test

--- cpp/ttt/main.cpp
+++ cpp/ttt/main.cpp
@@ -8,6 +8,9 @@ std::array<std::array<char, 3>,3> matrix = {{
 {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}}
 
 }; // 9 elements
+char player = 'X';
+//defining who's turn it is 
+
 void Draw() {
   std::cout<< " Tic Tac Toe V1.0"<< std::endl;
   for (int i =0; i <3; i++)
@@ -17,6 +20,18 @@ void Draw() {
     }
       std::cout<< "\n";// cout statement here to print out like a matrix
     // after printing one first row, it will add a new line and so on
+  }
+}
+// create function for input
+void Input(){
+  int input;
+  //print message 
+  std::cout<< "Enter your choice : ";
+  std::cin>> input;
+  
+  if( input ==1){
+
+    std::cout<< " hello";
   }
 }
 int main() {
